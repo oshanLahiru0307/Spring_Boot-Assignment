993c52c50e3c5f34a6424f13b48c2364e7b981e6
oshanLahiru0307 <klahiruoshan@gmail.com>
2025-08-10 19:37:40 +0530

    register request controller created in authcontroller

--- Backend/demo/src/main/java/com/example/demo/Controllers/AuthController.java
+++ Backend/demo/src/main/java/com/example/demo/Controllers/AuthController.java
@@ -1,4 +1,36 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.DTO.LoginRequest;
+import com.example.demo.DTO.RegisterRequest;
+import com.example.demo.Services.JWTService;
+import com.example.demo.Services.UserServices;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/auth")
 public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JWTService jwtUtil;
+    private final UserServices userService;
+
+    public AuthController(AuthenticationManager authenticationManager, JWTService jwtUtil, UserServices userService) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody RegisterRequest request) {
+        userService.createUser(request);
+        return "User registered!";
+    }
+
+
+
 }
