642e8ef4d3dff0969670845bd869f8a0374ff7d8
oshanLahiru0307 <klahiruoshan@gmail.com>
2025-08-10 19:11:59 +0530

    validateTOken function in JWTService  is completed

--- Backend/demo/src/main/java/com/example/demo/Services/JWTService.java
+++ Backend/demo/src/main/java/com/example/demo/Services/JWTService.java
@@ -47,6 +47,14 @@ public class JWTService {
     }
 
 
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretkey).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
 
 
