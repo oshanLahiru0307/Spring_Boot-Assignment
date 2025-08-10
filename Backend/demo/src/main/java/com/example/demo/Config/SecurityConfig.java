eb03d071f5dcc4ce17f70730d757f2e3966ee191
oshanLahiru0307 <klahiruoshan@gmail.com>
2025-08-10 19:02:39 +0530

    password encoder bean is created

--- Backend/demo/src/main/java/com/example/demo/Config/SecurityConfig.java
+++ Backend/demo/src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.example.demo.Config;
 
 import jakarta.annotation.Nonnull;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -25,5 +28,8 @@ public class SecurityConfig {
                 .build();
     }
 
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
